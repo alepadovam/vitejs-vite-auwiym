82dcad5ba523a1f172fa758b4d92274eb177138e
alepadovam <alepadovam@gmail.com>
2022-06-27 08:46:31 -0300

    1 file modified, 3 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import { useState } from 'react';
 import { Tweet } from './components/Tweet';
+import { AppRoutes } from './Routes';
 
 function App() {
   // return <h1> Hello SRTN/Unicamp</h1>;
-  const [tweets, setTweets] = useState<string[]>([
-    'Tweet 1',
-    'Tweet 2',
-    'Tweet 3',
-  ]);
+  // const [tweets, setTweets] = useState<string[]>([
+  //   'Tweet 1',
+  //   'Tweet 2',
+  //   'Tweet 3',
+  // ]);
 
-  function createTweet() {
-    setTweets([...tweets, 'Tweet 5']);
-  }
-  return (
-    <div>
-      {tweets.map((tweet) => {
-        return <Tweet text={tweet} />;
-      })}
-      <button onClick={createTweet}>Tweetar</button>
-    </div>
-  );
+  // function createTweet() {
+  //   setTweets([...tweets, 'Tweet 5']);
+  // }
+  // return (
+  //   <div>
+  //     {tweets.map((tweet) => {
+  //       return <Tweet text={tweet} />;
+  //     })}
+  //     <button onClick={createTweet}>Tweetar</button>
+  //   </div>
+  // );
+  return <AppRoutes />;
 }
 
 export default App;
